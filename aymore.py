2eaa2513c3d5f83a6d40d992e8af6855b4144798
Ederson <edersonrp@gmail.com>
2022-05-20 11:02:43 -0300

    acerto data e hora

--- aymore.py
+++ aymore.py
@@ -3,58 +3,72 @@ import os
 arquivos = os.listdir()
 cotacoes = []
 
-if arquivos
+excecao = ['aymore.py', '.gitignore', '.git', 'resultados.txt', 'novo']
+
+try:
+    for exc in excecao:
+
+        if exc in excecao:
+            arquivos.remove(exc)
+        else:
+            None
+
+except exc as error:
+    print(f'Deu ruim {error}')
 
 
-if ('sample_data') in arquivos:
-  arquivos.remove('sample_data')
-if ('.config') in arquivos:
-  arquivos.remove('.config') 
-if('./aymore.py'):
-  arquivos.remove('aymore.py')
-else:
-  None
-  
+def aymore(file: str):
+    horario = ''
+    data = ''
+    dia = ''
+    mes = ''
+    ano = ''
+    hora = ''
+    minuto = ''
+    produtos = []
+    pedido = ''
+    qtde = ''
+    qtde_produto = []
+    cod_cli = ''
+    pedido_consulta = ''
+    with open(file, mode='r', encoding='utf-8') as fp:
+        linha = fp.readline()
+        while linha:
+            if linha.startswith('0001'):
+                cod_cli = int(linha[6:19])
+                data = str(linha[20:28])
+                dia = data[:2]
+                mes = data[2:4]
+                ano = data[4:8]
+                horario = str(linha[28:32])
+                hora = horario[:2]
+                minuto = horario[2:4]
+                pedido = int(linha[32:38])
+                pedido_consulta = int(linha[38:40])
 
-def aymore(file:str):
-  data = ''
-  hora = ''
-  produtos = []
-  pedido = ''
-  qtde = ''
-  qtde_produto = []
-  pedido_consulta = ''
-  with open(file, mode='r', encoding='utf-8') as fp:
-    linha = fp.readline()
-    while linha:
-      if linha.startswith('0001'):
-        data = str(linha[20:28])
-        hora = str(linha[28:32])
-        pedido = int(linha[32:38])
-        pedido_consulta = int(linha[38:40])
-        
-      if linha[4:6] == '02':
-        posicao = linha[0:4]
-        produto = int(linha[6:20] )
-        produtos.append(produto)
-        qtde = int(linha[32:36])
-        qtde_produto.append(qtde)
-      linha = fp.readline()
+            if linha[4:6] == '02':
+                posicao = linha[0:4]
+                produto = int(linha[6:20])
+                produtos.append(produto)
+                qtde = int(linha[32:36])
+                qtde_produto.append(qtde)
+            linha = fp.readline()
 
-  print(f'Pedido: {pedido}')
-  print(f'Data: {data}')
-  print(f'Hora: {hora}')
-  print(f'Pedido consulta: {pedido_consulta}')
-  print('-' * 32 )
-  for produto, qtde in zip(produtos, qtde_produto):
-    print(f'ID: {produto} Qtd: {qtde}')
-  print('-' * 32 )
-  return ''
+    print('-' * 50)
+    print(f'|Pedido: {pedido} | Arquivo: {file}|')
+    print('-' * 50)
+    print(f'Data: {dia}/{mes}/{ano} Hora: {hora}:{minuto}')
+    print(f'Cliente: {cod_cli}')
+    print(f'Pedido consulta: {pedido_consulta}')
+    print('-' * 32)
+    for produto, qtde in zip(produtos, qtde_produto):
+        print(f'ID: {produto} Qtd: {qtde}')
+    print('-' * 32)
+    return ''
 
-def imprimir():
-  for arquivo in arquivos:
-    
-    print(arquivo)
+
+for arquivo in arquivos:
     print(aymore(arquivo))
 
-imprimir()
+
+print(f'{len(arquivos)} arquivos')
